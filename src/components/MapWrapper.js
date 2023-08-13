4228f2d418d01c51585860d048f39d8e8e53a745
Taylor Callsen <tcallsen@adobe.com>
2023-08-13 10:04:49 -0700

    adding wtf write transaction logic

--- src/components/MapWrapper.js
+++ src/components/MapWrapper.js
@@ -1,5 +1,5 @@
 // react
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 // openlayers
 import Map from 'ol/Map'
@@ -14,14 +14,72 @@ import {bbox as bboxStrategy} from 'ol/loadingstrategy.js';
 import WFS from 'ol/format/WFS';
 import GML from 'ol/format/GML32';
 
+import {Buffer} from 'buffer';
+
+const GEOSERVER_BASE_URL = 'http://localhost:8600/geoserver/dev';
+
 function MapWrapper(props) {
 
-  // refs are used instead of state to allow integration with 3rd party onclick callback;
+  const [featureData, setFeatureData] = useState();
+
+  // refs are used instead of state to allow integration with 3rd party map onclick callback;
   //  these are assigned at the end of the onload hook
   //  https://stackoverflow.com/a/60643670
-  const mapRef = useRef()
-  const mapElement = useRef() // remove
+  const mapRef = useRef();
+  const mapElement = useRef();
   const featuresLayerRef = useRef();
+
+  // map click handler - uses state and refs available in closure
+  const handleMapClick = async (event) => {
+
+    // get clicked feature from wfs layer
+    // TODO: currently only handles a single feature
+    const clickedCoord = mapRef.current.getCoordinateFromPixel(event.pixel);
+    const clickedFeatures = featuresLayerRef.current.getSource().getFeaturesAtCoordinate(clickedCoord);
+    if (!clickedFeatures.length) return; // exit callback if no features clicked
+    const feature = clickedFeatures[0];
+
+    // parse feature properties
+    const featureData = JSON.parse(feature.getProperties()['data']);
+
+    // iterate prop to test write-back
+    if (featureData.iteration) {
+      ++featureData.iteration;
+    } else featureData.iteration = 1;
+
+    // set property data back to feature
+    feature.setProperties({ data: JSON.stringify(featureData) });
+    console.log('clicked updated feature data', feature.getProperties())
+  
+    // prepare feature for WFS update transaction
+    //  https://dbauszus.medium.com/wfs-t-with-openlayers-3-16-6fb6a820ac58
+    const wfsFormatter = new WFS();
+    const gmlFormatter = new GML({
+      featureNS: GEOSERVER_BASE_URL,
+      featureType: 'generic',
+      srsName: 'EPSG:3857' // srs projection of map view
+    });
+    var xs = new XMLSerializer();
+    const node = wfsFormatter.writeTransaction(null, [feature], null, gmlFormatter);
+    var payload = xs.serializeToString(node);
+
+    // execute POST
+    await fetch(GEOSERVER_BASE_URL + '/wfs', {
+      headers: new Headers({
+        'Authorization': 'Basic ' + Buffer.from('admin:myawesomegeoserver').toString('base64'),
+        'Content-Type': 'text/xml'
+      }),
+      method: 'POST',
+      body: payload
+    });
+
+    // clear wfs layer features to force reload from backend to ensure latest properties
+    //  are available
+    featuresLayerRef.current.getSource().refresh();
+
+    // display updated feature data on map
+    setFeatureData(JSON.stringify(featureData));
+  }
 
   // initialize map on first render - logic formerly put into componentDidMount
   useEffect( () => {
@@ -31,7 +89,7 @@ function MapWrapper(props) {
       format: new GeoJSON(),
       url: function (extent) {
         return (
-          'http://localhost:8600/geoserver/dev/ows?service=WFS&' + 
+          GEOSERVER_BASE_URL + '/ows?service=WFS&' +
           'version=1.0.0&request=GetFeature&typeName=dev%3Ageneric&maxFeatures=50&' + 
           'outputFormat=application%2Fjson&srsname=EPSG:3857&' +
           'bbox=' +
@@ -92,57 +150,21 @@ function MapWrapper(props) {
   // render component
   return (      
     <div>
+
       <div ref={mapElement} className="map-container"></div>
+
+      { featureData ?
+        (
+        <div className="feature-data-display">
+          <p>Feature data: {featureData}</p>
+        </div>
+        ) :
+        null
+      }
+
     </div>
   ) 
 
-}
-
-// map click handler
-const handleMapClick = async (event, map, featuresLayer) => {
-    
-  // get clicked feature from wfs layer
-  // TODO: currently hard coded to a single feature
-  const clickedCoord = map.getCoordinateFromPixel(event.pixel);
-  const feature = featuresLayer.getSource().getFeaturesAtCoordinate(clickedCoord)[0]
-
-  // parse properties
-  const featureData = JSON.parse(feature.getProperties()['data']);
-
-  // iterate prop
-  if (featureData.iteration) {
-    ++featureData.iteration;
-  } else featureData.iteration = 1;
-
-  // set property data back to feature
-  feature.setProperties({ data: JSON.stringify(featureData) });
-  console.log('clicked updated feature data', feature.getProperties())
-
-  // prepare feature for WFS update transaction
-  //  https://dbauszus.medium.com/wfs-t-with-openlayers-3-16-6fb6a820ac58
-  const wfsFormatter = new WFS();
-  const gmlFormatter = new GML({
-    featureNS: 'http://localhost:8600/geoserver/dev',
-    featureType: 'generic',
-    srsName: 'EPSG:3857' // srs projection of map view
-  });
-  var xs = new XMLSerializer();
-  const node = wfsFormatter.writeTransaction(null, [feature], null, gmlFormatter);
-  var payload = xs.serializeToString(node);
-  
-  // execute POST
-  await fetch('http://localhost:8600/geoserver/dev/wfs', {
-    headers: new Headers({
-      'Authorization': 'Basic '+btoa('admin:myawesomegeoserver'), 
-      'Content-Type': 'text/xml'
-    }),
-    method: 'POST',
-    body: payload
-  });
-
-  // clear wfs layer features to force reload from backend to ensure latest properties
-  //  are available
-  featuresLayer.getSource().refresh();
 }
 
 export default MapWrapper
